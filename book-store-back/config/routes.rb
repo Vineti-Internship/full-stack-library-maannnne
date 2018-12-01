ee0548e5325d9ccaaf287a3408844f9ae48b2ac1
maannnne <mane.poghosian@gmail.com>
2018-12-01 14:13:01 +0400

    Add routes

--- book-store-back/config/routes.rb
+++ book-store-back/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :authors
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+        resources :authors, only: [:index, :create, :destroy, :update]
+    end
+  end
 end
