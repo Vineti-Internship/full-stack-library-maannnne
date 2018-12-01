a0f172b455120029ec687304c62fa9553a479dfb
maannnne <mane.poghosian@gmail.com>
2018-12-01 23:06:02 +0400

    Add context, component

--- book-store-front/src/App.js
+++ book-store-front/src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import Authors from './components/authors.js'
 import './App.css';
+import {ThemeContext, themes} from './components/context'
 
 class App extends Component {
+  state = {
+    theme: themes.dark,
+  }
+
   async componentDidMount(){
     const result = await fetch('http://localhost:3000/authors');
     console.log(await result.json());
   }
 
+
   render() {
     return (
-      <div className="App">
+      <ThemeContext.Provider value={this.state.theme}>
         <Authors/>
-      </div>
+      </ThemeContext.Provider>
     );
   }
 }
-
 export default App;
--- book-store-front/src/components/authors.js
+++ book-store-front/src/components/authors.js
@@ -1,33 +1,64 @@
 import React from 'react';
+import {ThemeContext, themes} from './context'
 
 class Authors extends React.Component {
   state = {
       authors: [],
-      books:[]
+      books: []
     }
 
-
-    async componentDidMount(){this.fetchAuthors();}
-    fetchAuthors = async() => {
+    async componentDidMount() {
       const result = await fetch('http://localhost:3000/api/v1/authors');
-      this.setState({authors: await result.json(),loaded:true})
-  };
+      const authors = await result.json();
+      this.setState({ authors, loaded: true });
+    };
 
-  render(){
-    const {authors} = this.state;
-    return(
-      {
-        authors.map()(
-          author => (
-            <div key={author.id}>
-              <h1>{author.fullName}</h1>
-              <p>{author.age}</p>
-            </div>
-          )
-        )
-      }
-    );
-  }
+  
+// render(){
+//   let props = this.props;
+//   //let theme = this.context;
+//   const { authors } = this.state;
+//   return (
+//     <div {...props} style={{backgroundColor: themes.background}}>
+//       {authors.map(author => (
+//         <div key={author.id}>
+//           <h1>{author.fullName}</h1>
+//           <p>{author.age}</p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
+
+
+
+  render() {
+    let props = this.props;
+    //let theme = this.context;
+    const { authors } = this.state;  
+      return (
+        <React.Fragment {...props} style={{backgroundColor: themes.background}}>  
+            <table>
+            <tr> 
+                <th class='tbb'>ID</th>
+                <th class='tbb'>Author</th> 
+                <th class='tbb'>Age</th>
+            </tr>
+            {authors.map(author => (
+             <React.Fragment key={author.id}>
+             <tr>
+                <td class='tbb'>{author.id}</td>
+                <td class='tbb'>{author.fullName}</td>
+                <td class='tbb'>{author.age}</td>
+             </tr>
+             </React.Fragment>
+
+            ))}
+            </table>
+        </React.Fragment>
+);
 }
-
+}
+Authors.contextType = ThemeContext;
 export default Authors;    
